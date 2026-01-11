40365688ce9e6ce8abea9245f36c02b3b58d7d7e
dens <snowmandxp@qq.com>
2026-01-11 10:21:15 +0800

    规范化Java文件版权头
    - 修复18个Java文件的版权头格式
    - 统一使用MIT License完整版权声明
    - 确保所有186个Java文件符合project-guideline.md规范

--- terra-ems-admin/src/main/java/com/terra/ems/admin/controller/SmsController.java
+++ terra-ems-admin/src/main/java/com/terra/ems/admin/controller/SmsController.java
@@ -1,7 +1,5 @@
 /*
- * MIT License
- *
- * Copyright (c) 2024 Terra EMS
+ * Copyright (c) 2024 泰若科技（广州）有限公司. All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -10,21 +8,23 @@
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
  */
 
 package com.terra.ems.admin.controller;
 
 import com.terra.ems.common.domain.Result;
+import com.terra.ems.framework.controller.Controller;
 import com.terra.ems.framework.domain.dto.SmsCodeRequest;
 import com.terra.ems.framework.domain.dto.SmsLoginRequest;
 import com.terra.ems.framework.service.SmsService;
@@ -49,9 +49,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * 短信相关接口
+ * Name: SmsController.java
+ * Email: dengxueping@gmail.com
+ * Date: 2026-01-11
+ * Description: 短信相关接口控制器
  *
- * @author Terra EMS
+ * @author dengxueping
  */
 @RestController
 @RequestMapping("/sms")
--- terra-ems-admin/src/main/java/com/terra/ems/ems/controller/PeakValleyController.java
+++ terra-ems-admin/src/main/java/com/terra/ems/ems/controller/PeakValleyController.java
@@ -1,7 +1,26 @@
 /*
- * MIT License
- * Copyright (c) 2024-2026 Terra EMS
+ * Copyright (c) 2024 泰若科技（广州）有限公司. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
  */
+
 package com.terra.ems.ems.controller;
 
 import com.terra.ems.common.domain.Result;
@@ -9,6 +28,7 @@ import com.terra.ems.ems.entity.TimePeriodPrice;
 import com.terra.ems.ems.service.PeakValleyAnalysisService;
 import com.terra.ems.ems.service.PeakValleyAnalysisService.DailyPeakValleyData;
 import com.terra.ems.ems.service.PeakValleyAnalysisService.PeakValleyAnalysisResult;
+import com.terra.ems.framework.controller.Controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -21,9 +41,12 @@ import java.time.YearMonth;
 import java.util.List;
 
 /**
- * 尖峰平谷分析控制器
+ * Name: PeakValleyController.java
+ * Email: dengxueping@gmail.com
+ * Date: 2026-01-11
+ * Description: 尖峰平谷分析控制器
  *
- * @author Terra EMS
+ * @author dengxueping
  */
 @Tag(name = "尖峰平谷分析")
 @RestController
